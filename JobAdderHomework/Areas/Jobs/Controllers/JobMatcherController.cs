fb54c5c86371d57f01836e5dd5843697e4d5004f
Anastasiia G <veydaster@gmail.com>
2018-05-27 21:15:15 +1000

    Added page structure

---
+++ JobAdderHomework/Areas/Jobs/Controllers/JobMatcherController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobAdderHomework.Areas.Jobs.Controllers
+{
+    public class JobMatcherController : Controller
+    {
+        // GET: Jobs/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
